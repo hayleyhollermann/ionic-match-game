4409a011c01d5790d9ef12801539074bbffef403
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-03-09 14:29:58 -0500

    randomizes dog photos

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -2,22 +2,55 @@ import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { shuffle } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
+
+  const images = [
+    { id: 1, image: 'https://images.unsplash.com/photo-1580650165001-550c2dcf0fff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2220&q=80' },
+    { id: 2, image: 'https://images.unsplash.com/photo-1580650165001-550c2dcf0fff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2220&q=80' },
+    { id: 3, image: 'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80' },
+    { id: 4, image: 'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80' },
+    { id: 5, image: 'https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80' },
+    { id: 6, image: 'https://images.unsplash.com/photo-1453227588063-bb302b62f50b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80' },
+    { id: 7, image: 'https://images.unsplash.com/photo-1557973964-ee879969b96d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80' },
+    { id: 8, image: 'https://images.unsplash.com/photo-1557973964-ee879969b96d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80' },
+  ];
+
+  const shuffle = () => {
+    let ctr = images.length, temp, index;
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+      // And swap the last element with it
+      temp = images[ctr];
+      images[ctr] = images[index];
+      images[index] = temp;
+    }
+    return images;
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Match</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Match</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className="match">
+          {shuffle().map((number) => 
+            <div key={number.id}><img src={number.image} /></div>
+          )}
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -8,13 +8,13 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Top Scores</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Top Scores</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 3 page" />
